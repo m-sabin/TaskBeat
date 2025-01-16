8aff9b09d2c849ad725857d393a8a4ba8c7934b9
nibass <contato.ecomcenter@gmail.com>
2025-01-16 11:58:54 -0300

    criar CategoryDao.kt, CategoryEntity.kt e TaskBeatDataBase.kt Inserir as categorias fixas do MainActivity.kt na base de dados TaskBeatDataBase.kt

---
+++ app/src/main/java/com/devspace/taskbeats/TaskBeatDataBase.kt
@@ -0,0 +1,13 @@
+package com.devspace.taskbeats
+
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database([CategoryEntity::class], version = 1)
+abstract class TaskBeatDataBase : RoomDatabase() {
+
+    abstract fun getCategoryDao(): CategoryDao
+
+
+}
